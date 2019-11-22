1741a5b289132d8f5c31aa5ecaf3080414fc1738
camillewalters <camille.h.walters@gmail.com>
2019-11-21 19:02:25 -0500

    final

--- project2.m
+++ project2.m
@@ -1,5 +1,7 @@
 clear all
+close all
 clc
+%Part A
 
 %Part A: displacement at each node
 %stiffness matrix
@@ -20,6 +22,7 @@ kBoundary=[0.75 -0.5 0 0 0;
 %force vector
 F=[0;0;0;0;2];
 
+%solving for the x values
 x=kBoundary\F
 
 %Part B: plot displacement as function of position
@@ -35,7 +38,8 @@ node6=x(5);
 
 displacements=[node1 node2 node3 node4 node5 node6]
 
-figure()
+%plotting the displacements and positions
+figure(1)
 plot(positions, displacements)
 title('Displacement as a function of position');
 xlabel('Original Position from Wall');
@@ -43,3 +47,32 @@ ylabel('Displacement');
 
 %Total displacement
 totalDisplacement=x(1)+x(2)+x(3)+x(4)+x(5)
+
+%Part B
+
+% PART A (find displacements at each node point)
+
+% 3x3 matrix for the system of equations for the Stepped Bar question
+K = [603.75e6*pi -138e6*pi 0;
+    -138e6*pi 163.875e6*pi -25.875e6*pi;
+     0 -25.875e6*pi 25.875e6*pi];
+
+P = [0; 0; 1000];
+
+phi = linsolve(K,P) %solve for individual nodal displacement
+
+
+% PART B (plot displacement for all locations)
+pos_from_wall = [0 0.1 0.25 0.45];
+
+displacement = [0 phi(1) phi(2) phi(3)];
+
+figure(2)
+plot(pos_from_wall, displacement)
+title('Displacement for all locations on the bar as a function of position');
+xlabel('position from wall [m]');
+ylabel('displacement [m]');
+
+% PART C (calculate the total displacement at end of bar)
+total_disp = (phi(1)+phi(2)+phi(3))*1000
+    %outputs total displacement at the end of the bar in cm
